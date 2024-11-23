097d84c251a401f6d83d29589c462ae210916c05
Lily-world <20000024iii@gmail.com>
2024-11-23 10:00:58 +0900

    2024/11/23
    11

---
+++ mmm_11.py
@@ -0,0 +1,23 @@
+#1
+#print("It's my life.")
+
+#2
+#print("'소리질러'~ \"화이팅!\"") # ''의 기능을 없애주려고 \ 사용
+
+#4
+#print("맛난 거\n식사류\t칡냉면\t300원")
+
+#5
+a = '오늘은'
+b = '신나는'
+c = input("기념일명: ")       #input 함수는 입력
+#print(a, b, c)  
+#print(f"{a} {b} {c}")  
+#print(a, b, "{}".format(c)) 
+#print(a, "%s %s" % (b, c))  
+#print(a+' '+b+' '+c)  
+
+#
+
+#
+str_ = "We can do it!"
